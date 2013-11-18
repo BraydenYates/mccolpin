f1fad26f918ba3d1cc431179a521a88ac064bb26
yatesschool <braydengyates2@gmail.com>
2013-11-18 17:13:17 -0600

    Update main.py
    Clear Screen! Bug and time fixes!

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 
-
 #IMPORTS
 
 import sys, time
-#import tkinter, ttysx
+#import _tkinter, ttysx
 
 #PASSION PROJECT
 p = raw_input("Select A Project. 1 = Passion, 2 = N/A")
@@ -14,15 +13,19 @@ if p == "1":
   if q == "2":
     print("My passion is coding")
     #THIS LOGICALLY WAITS 3 SECONDS...
-    time.sleep(3)
+    time.sleep(2)
     #IMAGE THAT PERFECTLY DEPICTS WHAT IS GOING ON AT THIS MOMENT
     #CITIATION THAT PERFECTLY DEPICTS SAID IMAGE
   
     print("I would like to put it into a career as a software developer and/or indie game developer.");
-    time.sleep(5)
-    print("The question, however, is:")
     time.sleep(3)
+    print("The question, however, is:")
+    time.sleep(2)
     print("How does this align with physics?")
+    time.sleep(2)
+    #CLEARS SCREEN
+    print "\n" * 80
     
+    #PROGRAM NUMBER 2 - TO BE ADDED
 if p == "2":
   print("There is no code here except this, so nothing will happen... Except this...")
