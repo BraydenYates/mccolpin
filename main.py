49dc1575cd2c1a9f3547248a40c9aad1e2cb7f18
yatesschool <braydengyates2@gmail.com>
2013-11-18 16:57:28 -0600

    Create main.py
    Bugs fixed. Moved from earlier ~/passion/ to ~/

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+#IMPORTS
+
+import sys, time
+#import tkinter, ttysx
+
+#PASSION PROJECT
+p = raw_input("Select A Project. 1 = Passion, 2 = N/A")
+if p == "1":
+
+  #DOING SHORT TERM TERMINAL BASE
+  q = raw_input()
+  if q == "2":
+    print("My passion is coding")
+    #THIS LOGICALLY WAITS 3 SECONDS...
+    time.sleep(3)
+    #IMAGE THAT PERFECTLY DEPICTS WHAT IS GOING ON AT THIS MOMENT
+    #CITIATION THAT PERFECTLY DEPICTS SAID IMAGE
+  
+    print("I would like to put it into a career as a software developer and/or indie game developer.");
+    time.sleep(5)
+    print("The question, however, is:")
+    time.sleep(3)
+    print("How does this align with physics?")
+    
+if p == "2":
+  print("There is no code here except this, so nothing will happen... Except this...")
