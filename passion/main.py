b88d6f3e035c6616f97d114ae76fe39997104a21
yatesschool <braydengyates2@gmail.com>
2013-11-18 16:58:48 -0600

    File is now outdated..
    OUTDATED FILE

--- passion/main.py
+++ passion/main.py
@@ -1,20 +1,30 @@
+
+# THIS IS OUTDATED!
+
+
+
+
+
+
+
+
 #IMPORTS
 
-import tkinter, sys, time, ttysx
+#import tkinter, sys, time, ttysx
 
 #DOING SHORT TERM TERMINAL BASE
-q = raw_input()
-if q = "1":
-  print("My passion is coding")
-  #THIS LOGICALLY WAITS 3 SECONDS...
-   time.sleep(3)
+#q = raw_input()
+#if q = "1":
+#  print("My passion is coding")
+#  #THIS LOGICALLY WAITS 3 SECONDS...
+#   time.sleep(3)
   #IMAGE THAT PERFECTLY DEPICTS WHAT IS GOING ON AT THIS MOMENT
   #CITIATION THAT PERFECTLY DEPICTS SAID IMAGE
   
-  print("I would like to put it into a career as a software developer and/or indie game developer.");
-  time.sleep(5)
-  print("The question, however, is:")
-  time.sleep(3)
-  print("How does this align with physics?")
+#  print("I would like to put it into a career as a software developer and/or indie game developer.");
+#  time.sleep(5)
+#  print("The question, however, is:")
+#  time.sleep(3)
+#  print("How does this align with physics?")
   
   
