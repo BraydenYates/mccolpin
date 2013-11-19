548eb4f7eaeec53e00ee2f646101f242b28f9e32
BraydenYates <braydengyates@aol.com>
2013-11-19 16:24:11 -0600

    Create cli.py

---
+++ cli.py
@@ -0,0 +1,38 @@
+#IMPORTS
+
+import sys, time
+#import _tkinter, ttysx
+
+#PASSION PROJECT
+p = raw_input("Select A Project. 1 = Passion, 2 = N/A")
+if p == "1":
+
+  #DOING SHORT TERM TERMINAL BASE
+  q = raw_input()
+  if q == "1":
+    print("My passion is coding")
+    #THIS LOGICALLY WAITS 3 SECONDS...
+    time.sleep(2)
+    #IMAGE THAT PERFECTLY DEPICTS WHAT IS GOING ON AT THIS MOMENT
+    #CITIATION THAT PERFECTLY DEPICTS SAID IMAGE
+  
+    print("I would like to put it into a career as a software developer and/or indie game developer.");
+    time.sleep(3)
+    print("The question, however, is:")
+    time.sleep(2)
+    print("How does this align with physics?")
+    time.sleep(2)
+    #CLEARS SCREEN
+    print "\n" * 42
+    print(" How this aligns with physics")
+    #17 TABS FOR CENTER
+    print "\n" * 21
+
+    time.sleep(5)
+    print "\n" * 42
+    print(" ACCELERATION")
+    print "\n" * 21
+
+    #PROGRAM NUMBER 2 - TO BE ADDED
+if p == "2":
+  print("There is no code here except this, so nothing will happen... Except this...")
