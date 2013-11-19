6574f72d189a871e59e2d5373aee8ed03dfed862
BraydenYates <braydengyates@aol.com>
2013-11-19 16:59:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,19 @@
 from Tkinter import *
 
 root = Tk()
+#Title, duh. Look at the line!
+root.title("Passion Project")
 
 var = StringVar()
 label = Label( root, textvariable=var, relief=RAISED )
-
+#Introduction
 var.set("My passion is coding. Someday, I'd love to get a job developing software or being an indie game developer. \n The question, however, is how can I align this with physics? ")
-
-def one():
+#Functions 
+def acceleration():
     var.set("Acceleration \n \n \n \n \n \n Acceleration is defined as: \n 'the increase in the rate or speed of something. \n How could this be used in code? \n \n \n'")
 
-b = Button(root, text="Continue", command=one)
+
+b = Button(root, text="Acceleration", command=acceleration)
 b.pack()
 
 
