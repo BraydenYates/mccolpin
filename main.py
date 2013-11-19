18db434a936f0a38b3ada19311d7a6efcd92b64e
BraydenYates <braydengyates@aol.com>
2013-11-19 16:28:53 -0600

    Update main.py
    Revamped to include GUI

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ label = Label( root, textvariable=var, relief=RAISED )
 var.set("My passion is coding. Someday, I'd love to get a job developing software or being an indie game developer. \n The question, however, is how can I align this with physics? ")
 
 def one():
-    var.set("Acceleration \n \n \n Here is information on how acceleration is implemented into coding.")
+    var.set("Acceleration \n \n \n \n \n \n Acceleration is defined as: \n 'the increase in the rate or speed of something. \n How could this be used in code? \n \n \n'")
 
 b = Button(root, text="Continue", command=one)
 b.pack()
