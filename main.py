59065ddcc72a837a6711c3bbd8debccf392b6c0f
BraydenYates <braydengyates@aol.com>
2013-11-19 16:25:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
-#IMPORTS
+#from Tkinter import *
 
-import sys, time
-#import _tkinter, ttysx
+#master = Tk()
 
-#PASSION PROJECT
-p = raw_input("Select A Project. 1 = Passion, 2 = N/A")
-if p == "1":
+#def callback():
+#    print "click!"
 
-  #DOING SHORT TERM TERMINAL BASE
-  q = raw_input()
-  if q == "1":
-    print("My passion is coding")
-    #THIS LOGICALLY WAITS 3 SECONDS...
-    time.sleep(2)
-    #IMAGE THAT PERFECTLY DEPICTS WHAT IS GOING ON AT THIS MOMENT
-    #CITIATION THAT PERFECTLY DEPICTS SAID IMAGE
-  
-    print("I would like to put it into a career as a software developer and/or indie game developer.");
-    time.sleep(3)
-    print("The question, however, is:")
-    time.sleep(2)
-    print("How does this align with physics?")
-    time.sleep(2)
-    #CLEARS SCREEN
-    print "\n" * 42
-    print("                                                                  How this aligns with physics")
-    #17 TABS FOR CENTER
-    print "\n" * 21
+#b = Button(master, text="OK", command=callback)
+#b.pack()
 
-    time.sleep(5)
-    print "\n" * 42
-    print("                                                                   ACCELERATION")
-    print "\n" * 21
+#mainloop()
 
-    #PROGRAM NUMBER 2 - TO BE ADDED
-if p == "2":
-  print("There is no code here except this, so nothing will happen... Except this...")
+from Tkinter import *
+
+root = Tk()
+
+var = StringVar()
+label = Label( root, textvariable=var, relief=RAISED )
+
+var.set("My passion is coding. Someday, I'd love to get a job developing software or being an indie game developer. \n The question, however, is how can I align this with physics? ")
+
+def one():
+    var.set("Acceleration \n \n \n Here is information on how acceleration is implemented into coding.")
+
+b = Button(root, text="Continue", command=one)
+b.pack()
+
+
+label.pack()
+root.mainloop()
