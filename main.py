7f02f4702660c8c36fd1d6a3b76fba9d68ec2e09
BraydenYates <braydengyates@aol.com>
2013-11-19 18:56:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-#from Tkinter import *
-
-#master = Tk()
-
-#def callback():
-#    print "click!"
-
-#b = Button(master, text="OK", command=callback)
-#b.pack()
-
-#mainloop()
 
 from Tkinter import *
 
@@ -16,18 +5,38 @@ root = Tk()
 #Title, duh. Look at the line!
 root.title("Passion Project")
 
+#Other Variables
+
+photo = PhotoImage(file="accel.gif")
+img = Label(root, image=photo)
+img.photo = photo
+
+
+
+
+    
 var = StringVar()
 label = Label( root, textvariable=var, relief=RAISED )
 #Introduction
 var.set("My passion is coding. Someday, I'd love to get a job developing software or being an indie game developer. \n The question, however, is how can I align this with physics? ")
+
+
 #Functions 
 def acceleration():
     var.set("Acceleration \n \n \n \n \n \n Acceleration is defined as: \n 'the increase in the rate or speed of something. \n How could this be used in code? \n \n \n'")
+    img.pack()
 
 
-b1 = Button(root, text="Acceleration", command=acceleration)
-b1.pack()
+def newton():
+  var.set("Newton's Laws \n \n \n")
 
+#Acceleration Button!
+accel = Button(root, text='Acceleration', command=acceleration)
+accel.pack()
+
+#Newton's Button
+newton = Button(root, text="Newton's Laws", command=newton)
+newton.pack()
 
 label.pack()
 root.mainloop()
